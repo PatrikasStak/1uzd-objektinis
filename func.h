80d12a2e1ac42e65812ac283e3c7f41cd4e0a86d
Patrikas <patrestak@gmail.com>
2026-03-10 20:06:00 +0200

    rusiavimas galimas is terminalo

--- func.h
+++ func.h
@@ -19,6 +19,6 @@ double Mediana(const std::vector<Studentas>& X, int x);
 void Rezultatas(std::vector<Studentas>& X);
 void RezultatasFailo(std::vector<Studentas>& X);
 void FailuGeneravimas();
-void GeneruotuRusiavimas(string path)
+void GeneruotuRusiavimas(std::string path);
 
 #endif
