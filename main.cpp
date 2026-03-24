cf349731a1e702840860240fe5599c00b6591f87
Patrikas <patrestak@gmail.com>
2026-03-24 22:17:03 +0200

    callinamos templ maine

--- main.cpp
+++ main.cpp
@@ -21,20 +21,47 @@ int main() {
     else if(choice=="3"){
         FailuGeneravimas();
     }
-    else if(choice=="4"){
-        while(true){
-            std::cout<<"Kuri faila rusioti? 1 - generuoti1000.txt 2 - generuoti10000.txt 3 - generuoti100000.txt 4 - generuoti1000000.txt 5 - generuoti10000000.txt 6 - baigti \n";
-            if(choice=="1"||choice=="2"||choice=="3"||choice=="4"||choice=="5"||choice=="6"){
-            getline(std::cin, choice);
-            if(choice=="1")GeneruotuRusiavimas("generuoti1000.txt");
-            else if(choice=="2")GeneruotuRusiavimas("generuoti10000.txt");
-            else if(choice=="3")GeneruotuRusiavimas("generuoti100000.txt");
-            else if(choice=="4")GeneruotuRusiavimas("generuoti1000000.txt");
-            else if(choice=="5")GeneruotuRusiavimas("generuoti10000000.txt");
-            else if(choice=="6")break;
-            }
-            else std::cerr<<"Neteisinga ivestis! \n";
+else if (choice == "4") {
+    std::string kont;
+    std::cout << "Koki konteineri naudoti? 1 - vector, 2 - list, 3 - deque\n";
+    getline(std::cin, kont);
+
+    while (true) {
+        std::cout << "Kuri faila rusioti? 1 - generuoti1000.txt 2 - generuoti10000.txt 3 - generuoti100000.txt 4 - generuoti1000000.txt 5 - generuoti10000000.txt 6 - baigti \n";
+        getline(std::cin, choice);
+
+        if (choice == "1") {
+            if (kont == "1") GeneruotuRusiavimasVec("generuoti1000.txt");
+            else if (kont == "2") GeneruotuRusiavimasList("generuoti1000.txt");
+            else if (kont == "3") GeneruotuRusiavimasDeque("generuoti1000.txt");
+            else std::cerr << "Neteisingas konteinerio pasirinkimas\n";
+        } else if (choice == "2") {
+            if (kont == "1") GeneruotuRusiavimasVec("generuoti10000.txt");
+            else if (kont == "2") GeneruotuRusiavimasList("generuoti10000.txt");
+            else if (kont == "3") GeneruotuRusiavimasDeque("generuoti10000.txt");
+            else std::cerr << "Neteisingas konteinerio pasirinkimas\n";
+        } else if (choice == "3") {
+            if (kont == "1") GeneruotuRusiavimasVec("generuoti100000.txt");
+            else if (kont == "2") GeneruotuRusiavimasList("generuoti100000.txt");
+            else if (kont == "3") GeneruotuRusiavimasDeque("generuoti100000.txt");
+            else std::cerr << "Neteisingas konteinerio pasirinkimas\n";
+        } else if (choice == "4") {
+            if (kont == "1") GeneruotuRusiavimasVec("generuoti1000000.txt");
+            else if (kont == "2") GeneruotuRusiavimasList("generuoti1000000.txt");
+            else if (kont == "3") GeneruotuRusiavimasDeque("generuoti1000000.txt");
+            else std::cerr << "Neteisingas konteinerio pasirinkimas\n";
+        } else if (choice == "5") {
+            if (kont == "1") GeneruotuRusiavimasVec("generuoti10000000.txt");
+            else if (kont == "2") GeneruotuRusiavimasList("generuoti10000000.txt");
+            else if (kont == "3") GeneruotuRusiavimasDeque("generuoti10000000.txt");
+            else std::cerr << "Neteisingas konteinerio pasirinkimas\n";
+        } else if (choice == "6") {
+            break;
+        } else {
+            std::cerr << "Neteisinga ivestis!\n";
         }
     }
+}
+
     return 0;
 }
